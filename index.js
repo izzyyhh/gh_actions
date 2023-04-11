6f0eef5b3120a42a15f15cd929d2eccf77d665ce
Ismail Halili <ihalili.mmt-m2022@fh-salzburg.ac.at>
2023-04-11 16:32:39 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+console.log("hello izzy");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("POWER!");
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`server rennt auf ${PORT}.`);
+});
